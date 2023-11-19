2eca20e7748b00735d7a85df276b8ab569a1d78b
XAVIER MESNILDREY <xavier.msy@gmail.com>
2023-11-19 18:19:17 +0100

    Quete 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,41 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
 import { useState } from "react";
-import "./App.css"
-
+import "./App.css";
 
 const pokemonList = [
   {
-     name: "Bulbasaur",
-     imgSrc:
-       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-     pv: "Pv: 60",
-     attaque: "Attaque: 90",
-     defense: "Défense: 70",
-     nofind: "(Pokémon capturé)",
-     uncommon: "Pokémon peu-commun",
-   },
-   {
-     name: "Charmander",
-     imgSrc:
-       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-     pv: "Pv: 63",
-     attaque: "Attaque: 96",
-     defense: "Défense: 50",
-     nofind: "(Pokémon capturé)",
-     uncommon: "Pokémon peu-commun",
-   },
-   {
-     name: "Squirtle",
-     imgSrc:
-       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-       nofind: "(Pokémon aperçu)",
-       uncommon: "Pokémon peu-commun",
-   },
-   {
-     name: "Pikachu",
-     imgSrc:
-       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-     pv: "Pv: 80",
-     attaque: "Attaque: 110",
-     defense: "Défense: 95",
-     nofind: "(Pokémon capturé)",
-     rarity: "Pokémon rare",
-   },
-   {
-     name: "Mew",
-     nofind: "(Pokémon inconnu)",
-     legendary: "Pokémon légendaire",
-   },
+    id: 0,
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    id: 1,
+    name: "Charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    id: 2,
+    name: "Squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    id: 3,
+    name: "Pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    id: 4,
+    name: "Mew",
+  },
 ];
 
 function App() {
-
-  const [pokemonIndex, setPokemonIndex] = useState(0)
-  const precedent = () => {
-    setPokemonIndex (pokemonIndex - 1);
-  }
-  const suivant = () => {
-    setPokemonIndex (pokemonIndex + 1);
-  }
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
   const pokemon = pokemonList[pokemonIndex];
 
@@ -67,25 +45,13 @@ function App() {
         <h1>POKEDEX</h1>
       </div>
       <div>
-        <p className="bv-pokedex">Bienvenue sur ton Pokedex ! Ici tu retrouveras toutes les informations sur les pokémons que tu as pu croiser ou capturer lors de ton aventure.</p>
+        <NavBar tableau={pokemonList}fonction={setPokemonIndex} />
       </div>
       <div>
-        <NavBar 
-        pokemonI={pokemonIndex}
-        precedent={precedent}
-        suivant={suivant}
-        pokemon={pokemonList}/>
-      </div>
-      <div>
-        <PokemonCard pokemon={pokemon}/>
-      </div>
-      <div className="legend-color-p">
-        <p className="p-uncommon">Pokémon peu commun</p>
-        <p className="p-rarity">Pokémon rare</p>
-        <p className="p-legendary">Pokémon légendaire</p>
+        <PokemonCard pokemon={pokemon} />
       </div>
     </>
-  ); 
+  );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,20 @@
 import PropTypes from "prop-types";
 
-function NavBar (props) {
-
-    return (
-        <nav className="nav-style">
-            <p>Clique sur les boutons pour faire défiler les pokémons de ton pokedex</p>
-            {props.pokemonI > 0 &&
-                <button onClick={props.precedent}>Précédent</button>
-            }
-            {props.pokemonI < props.pokemon.length - 1 &&
-                <button onClick={props.suivant}>Suivant</button>
-            }
-        </nav>
-    )
+function NavBar({ tableau, fonction }) {
+  return (
+    <nav className="nav-style">
+      {tableau.map((pokemon) => (
+        <button key={pokemon.id} onClick={() => fonction(pokemon.id)}>
+          {pokemon.name}
+        </button>
+      ))}
+    </nav>
+  );
 }
 
 NavBar.propTypes = {
-    pokemonI : PropTypes.string.isRequired,
-    precedent : PropTypes.string.isRequired,
-    suivant : PropTypes.string.isRequired,
-    pokemon : PropTypes.shape({
-
-    }).isRequired,
-}
+  tableau: PropTypes.array.isRequired,
+  fonction: PropTypes.func.isRequired,
+};
 
 export default NavBar;
